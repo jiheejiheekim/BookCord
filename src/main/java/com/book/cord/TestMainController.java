57e28ffdc5f843f5604b76c2c9c5b13678797d40
jiheejiheekim <jihee2518@gmail.com>
2024-03-01 17:28:35 +0900

    API 테스트 - 신간도서 띄우기 ing

--- src/main/java/com/book/cord/TestMainController.java
+++ src/main/java/com/book/cord/TestMainController.java
@@ -1,63 +1,44 @@
 package com.book.cord;
 
-import java.io.IOException;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.client.RestTemplate;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 import lombok.Data;
 
 @Controller
 public class TestMainController {
 
-    @Autowired
-    private RestTemplate restTemplate;
-
     private static final String API_URL = "http://www.aladin.co.kr/ttb/api/ItemList.aspx";
     private static final String API_KEY = "ttbfldpf20031555001";
 
+	/*
+	 * @GetMapping("/testmain") 
+	 * public String main() { 
+	 * 		return "testmain"; 
+	 * }
+	 */
+
     @GetMapping("/testmain")
     public String main() {
-        return "testmain";
+        String newBooksJson = requestNewBooksFromAladdin();
+        return newBooksJson;
     }
 
-    @GetMapping("/search")
-    public String searchBooks(Model model) {
-
-        String url = API_URL + "?ttbkey=" + API_KEY + "&QueryType=ItemNewAll"
-                + "&MaxResults=10&start=1&SearchTarget=Book&output=json&Version=20131101";
-
-        String jsonResponse = restTemplate.getForObject(url, String.class);
-
-        // JSON 데이터를 SearchResult 객체로 변환
-        SearchResult searchResult = convertJsonToSearchResult(jsonResponse);
-
-        if (searchResult != null && searchResult.getBooks() != null && !searchResult.getBooks().isEmpty()) {
-            model.addAttribute("books", searchResult.getBooks());
-        } else {
-            model.addAttribute("books", null); // 검색 결과가 없을 경우, books를 null로 설정
-        }
-
-        return "search";
+    private String requestNewBooksFromAladdin() {
+        RestTemplate restTemplate = new RestTemplate();
+        String url = API_URL + "?ttbkey=" + API_KEY + "&QueryType=ItemNewAll&MaxResults=10&start=1&SearchTarget=Book&output=json&Version=20131101";
+        return restTemplate.getForObject(url, String.class);
+    }
+    
+    @GetMapping("/bc/view")
+    public String view() {
+        return "testmain"; // testmain.jsp 파일을 렌더링하여 응답으로 반환
     }
 
-    // JSON 데이터를 SearchResult 객체로 변환하는 메서드
-    private SearchResult convertJsonToSearchResult(String json) {
-        try {
-            ObjectMapper objectMapper = new ObjectMapper();
-            SearchResult searchResult = objectMapper.readValue(json, SearchResult.class);
-            return searchResult;
-        } catch (IOException e) {
-            e.printStackTrace();
-            return null; // 변환 실패 시 null 반환
-        }
-    }
 
     @Data
     public static class SearchResult {
@@ -66,7 +47,7 @@ public class TestMainController {
     }
 
     @Data
-    public class Book {
+    public static class Book {
         private String title;
         private String author;
         private String publisher;
--- src/main/webapp/WEB-INF/views/testmain.jsp
+++ src/main/webapp/WEB-INF/views/testmain.jsp
@@ -1,24 +1,25 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ page import="com.fasterxml.jackson.databind.ObjectMapper" %>
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Search Results</title>
+    <title>New Books</title>
 </head>
 <body>
-    <h2>Search Results</h2>
+    <h2>New Books</h2>
 
-    <c:if test="${empty books}">
-        <p>No search results found.</p>
+    <c:if test="${not empty newBooks}">
+        <%-- JSON 문자열을 JSON 객체로 변환 --%>
+        <c:set var="newBooksObj" value="<%= new ObjectMapper().readTree((String)request.getAttribute(\"newBooks\")) %>" />
+        <c:forEach items="${newBooksObj.item}" var="book">
+            <div>
+                <h3>Title: ${book.title}</h3>
+                <p>Author: ${book.author}</p>
+                <p>Publisher: ${book.publisher}</p>
+                <p>ISBN: ${book.isbn}</p>
+            </div>
+        </c:forEach>
     </c:if>
-
-    <c:forEach items="${books}" var="book">
-        <div>
-            <h3>Title: ${book.title}</h3>
-            <p>Author: ${book.author}</p>
-            <p>Publisher: ${book.publisher}</p>
-            <p>ISBN: ${book.isbn}</p>
-        </div>
-    </c:forEach>
 </body>
 </html>
